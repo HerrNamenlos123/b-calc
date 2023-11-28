bb528876308ba253c41db71cad5d948017e2954d
HerrNamenlos123 <herrnamenlos123@gmail.com>
2023-11-28 20:44:20 +0100

    Implement awesome animation frame requesting and idling

--- src/ui/main.lua
+++ src/ui/main.lua
@@ -1,13 +1,23 @@
 
+local i = 0
+local frame = false
+
 function mainWindow()
     if Button({text="Click me!"}) then
         print("Clicked")
+        frame = not frame
+        b.requestAnimationFrameIn(0)
     end
+    ImGui.Text(""..i)
+    i = i + 1
 end
-
 
 function render()
     ImGui.ShowDemoWindow()
+
+    if frame then
+        b.requestAnimationFrameIn(0)
+    end
 
     Window({title="Main Window"}, mainWindow)
 end
